8424b3e3f049cd11778bd4de16c4bb2c38ef1486
Janish888 <janish8008@gmail.com>
2026-02-26 19:34:00 +0530

    Updated allowed hosts

--- atm_project/settings.py
+++ atm_project/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-33d95.up.railway.app',
+    'localhost',
+    '127.0.0.1']
 
 
 # Application definition
